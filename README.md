6ac06ff4b14f208708f66fab34860c0e44452b27
kvoon <a1176079417@gmail.com>
2024-09-29 18:28:28 +0800

    first commit

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
-# ext-name
+# Fast Code Selector
 
-<a href="https://marketplace.visualstudio.com/items?itemName=antfu.ext-name" target="__blank"><img src="https://img.shields.io/visual-studio-marketplace/v/antfu.ext-name.svg?color=eee&amp;label=VS%20Code%20Marketplace&logo=visual-studio-code" alt="Visual Studio Marketplace Version" /></a>
+<a href="https://marketplace.visualstudio.com/items?itemName=kvoon.fast-code-selector" target="__blank"><img src="https://img.shields.io/visual-studio-marketplace/v/kvoon.fast-code-selector.svg?color=eee&amp;label=VS%20Code%20Marketplace&logo=visual-studio-code" alt="Visual Studio Marketplace Version" /></a>
 <a href="https://kermanx.github.io/reactive-vscode/" target="__blank"><img src="https://img.shields.io/badge/made_with-reactive--vscode-%23007ACC?style=flat&labelColor=%23229863"  alt="Made with reactive-vscode" /></a>
+
+Fast Code Selector allows you to quickly select and manipulate code blocks based on the structure of the code near your cursor.
+
+## Commands
+
+<!-- commands -->
+
+| Command                            | Title                                      |
+| ---------------------------------- | ------------------------------------------ |
+| `fastCodeSelector.fn.select`       | Fast Code Selector: Function Select        |
+| `fastCodeSelector.fn.delete`       | Fast Code Selector: Function Select        |
+| `fastCodeSelector.fnReturn.select` | Fast Code Selector: Function Return Select |
+| `fastCodeSelector.fnReturn.delete` | Fast Code Selector: Function Return Delete |
+| `fastCodeSelector.fnBody.select`   | Fast Code Selector: Function Body Select   |
+| `fastCodeSelector.fnBody.delete`   | Fast Code Selector: Function Body Delete   |
+
+<!-- commands -->
 
 ## Configurations
 
@@ -11,22 +28,12 @@
 
 <!-- configs -->
 
-## Commands
+## Roadmap
 
-<!-- commands -->
-
-**No data**
-
-<!-- commands -->
-
-## Sponsors
-
-<p align="center">
-  <a href="https://cdn.jsdelivr.net/gh/antfu/static/sponsors.svg">
-    <img src='https://cdn.jsdelivr.net/gh/antfu/static/sponsors.png'/>
-  </a>
-</p>
+- [ ] More ast node
+  - [x] Function
+- [ ] Vue support
 
 ## License
 
-[MIT](./LICENSE.md) License © 2022 [Anthony Fu](https://github.com/antfu)
+[MIT](./LICENSE.md) License © 2024 [Kevin Kwong](https://github.com/kvoon3)
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,208 @@
-import { defineExtension } from 'reactive-vscode'
-import { window } from 'vscode'
+import type { BlockStatement, Node, ReturnStatement } from '@babel/types'
+import type { TextDocument } from 'vscode'
+import { parse } from '@babel/parser'
+import traverse from '@babel/traverse'
+import { computed, defineExtension, useActiveTextEditor, useCommand, useDocumentText, useTextEditorSelection } from 'reactive-vscode'
+import { Position, Range, Selection } from 'vscode'
+import { commands } from './generated/meta'
+import { logger } from './utils'
+
+type FnOperator = (nodes: ReturnType<typeof getFn>) => any
 
 const { activate, deactivate } = defineExtension(() => {
-  window.showInformationMessage('Hello')
+  logger.show()
+
+  const editor = useActiveTextEditor()
+  const doc = computed(() => editor.value?.document)
+  const text = useDocumentText(doc)
+  const editorSelection = useTextEditorSelection(editor)
+
+  function handler(operater: FnOperator) {
+    logger.info('trigger')
+
+    if (
+      !editor.value
+      || !doc.value
+      || !text.value
+      || !editorSelection.value
+    ) {
+      return
+    }
+
+    if (!['typescript', 'javascript'].includes(doc.value.languageId))
+      return
+
+    if (!text)
+      return
+
+    try {
+      if (
+        editorSelection.value
+        && doc.value
+      ) {
+        const nodes = getFn({
+          ast: parse(text.value, {
+            plugins: [
+              'typescript',
+            ],
+          }),
+          cursorSelection: editorSelection.value,
+          doc: doc.value,
+        })
+
+        operater(nodes)
+      }
+      else {
+        logger.error('no selection')
+      }
+    }
+    catch (error) {
+      logger.error('error', error)
+    }
+  }
+
+  useCommand(commands.fastCodeSelectorFnSelect, () => {
+    handler(({ expression }) => expression && selectNode(expression))
+  })
+
+  useCommand(commands.fastCodeSelectorFnDelete, () => {
+    handler(({ expression }) => expression && deleteNode(expression))
+  })
+
+  useCommand(commands.fastCodeSelectorFnBodySelect, () => {
+    handler(({ body }) => body && selectNode(body))
+  })
+
+  useCommand(commands.fastCodeSelectorFnBodyDelete, () => {
+    handler(({ body }) => body && deleteNode(body))
+  })
+
+  useCommand(commands.fastCodeSelectorFnReturnSelect, () => {
+    handler(({ returnStatement }) => returnStatement && selectNode(returnStatement))
+  })
+
+  useCommand(commands.fastCodeSelectorFnReturnDelete, () => {
+    handler(({ returnStatement }) => returnStatement && deleteNode(returnStatement))
+  })
 })
+
+function getFn(ctx: {
+  ast: Node
+  doc: TextDocument
+  cursorSelection: Selection
+}) {
+  const supportedNodeType = [
+    'FunctionDeclaration',
+    'ArrowFunctionExpression',
+    'ClassMethod',
+  ]
+
+  const { ast, doc, cursorSelection } = ctx
+
+  // let thePath: NodePath<FunctionDeclaration | ArrowFunctionExpression | ClassMethod> | undefined
+
+  let expression: Node | undefined
+  let expressionBody: BlockStatement | undefined
+  let returnStatement: ReturnStatement | undefined
+
+  traverse(ast, {
+    enter(path) {
+      if (path.node.start == null || path.node.end == null)
+        return
+
+      if (!supportedNodeType.includes(path.node.type)) {
+        // logger.info(`[js-arrow-fn] Unknown ${path.node.type}`)
+        return
+      }
+
+      logger.info('----------------------')
+
+      const {
+        node: {
+          start,
+          end,
+        },
+      } = path
+
+      const fnSelection = new Selection(
+        doc.positionAt(start!),
+        doc.positionAt(end!),
+      )
+
+      const cursorStartPos = new Position(
+        cursorSelection.start.line,
+        cursorSelection.start.character,
+      )
+
+      const cursorEndPos = new Position(
+        cursorSelection.end.line,
+        cursorSelection.end.character,
+      )
+
+      if (
+        fnSelection.start.isBeforeOrEqual(cursorStartPos)
+        && fnSelection.end.isAfterOrEqual(cursorEndPos)
+      ) {
+        // thePath = path
+        expression = path.node
+        // @ts-expect-error type err
+        expressionBody = path.node.body
+        // @ts-expect-error type err
+        returnStatement = path.node.body.body.find(i => i.type === 'ReturnStatement')
+        logger.info('expression', JSON.stringify(expression, null, 2))
+      }
+      else {
+        logger.info('skip')
+        return path.skip()
+      }
+    },
+  })
+
+  return {
+    expression,
+    body: expressionBody,
+    returnStatement,
+  }
+}
+
+function selectNode(node: Node) {
+  const editor = useActiveTextEditor()
+  if (!editor.value)
+    return
+
+  const { loc } = node
+  if (!loc)
+    return
+
+  if (!editor)
+    return
+
+  const start = new Position(loc.start.line - 1, loc.start.column)
+  const end = new Position(loc.end.line - 1, loc.end.column)
+  editor.value.selection = new Selection(start, end)
+}
+
+function deleteNode(node: Node) {
+  const editor = useActiveTextEditor()
+  if (!editor.value)
+    return
+
+  const { loc } = node
+  if (!loc)
+    return
+
+  const start = new Position(loc.start.line - 1, loc.start.column)
+  const end = new Position(loc.end.line - 1, loc.end.column)
+  const range = new Range(start, end)
+
+  editor.value.edit((editBuilder) => {
+    editBuilder.delete(range)
+  }).then((success) => {
+    if (success)
+      logger.info('Node deleted successfully')
+    else
+      logger.error('Failed to delete node')
+  })
+}
 
 export { activate, deactivate }
